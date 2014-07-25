f34a116ef927d59b32fef7806d9cfb9e22bf7c79
Roman <vollossy@gmail.com>
2014-07-25 09:28:22 +0400

    Инициализация репозитория.

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=15432;dbname=accounting',
+    'username' => 'postgres',
+    'password' => 'dbpass',
+    'charset' => 'utf8',
+];
---
+++ migrations/m140725_051529_init_db.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+/**
+ * Class m140725_051529_init_db
+ * Инициализирует бд с основными таблицами
+ */
+class m140725_051529_init_db extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('accounts', [
+            'client' => 'string NOT NULL',
+            'serial' => 'int NOT NULL',
+            'balance' => 'float NOT NULL DEFAULT 0',
+        ]);
+        $this->addPrimaryKey('pk_serial', 'accounts', 'serial');
+        $this->insert('accounts', ['client' => 'Наша фирма', 'serial' => 0]);
+    }
+
+    public function down()
+    {
+        echo "m140725_051529_init_db cannot be reverted.\n";
+
+        return false;
+    }
+}
